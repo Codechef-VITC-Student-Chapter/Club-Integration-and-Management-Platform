f324bcc3419ef2e4ed5f83dbc43f832451ffb48b
Azaken1248 <suvkrm@gmail.com>
2024-08-16 01:46:05 +0530

    Added functions to edit clubs and departments of a user

--- Server/src/utils/userUtils.mjs
+++ Server/src/utils/userUtils.mjs
@@ -7,13 +7,13 @@ dotenv.config();
 const username = process.env.MONGO_USERNAME;
 const password = process.env.MONGO_PASSWORD;
 
-const connectionString = `mongodb+srv://${username}:${password}@ciandmp.duflp.mongodb.net/?retryWrites=true&w=majority&appName=CIandMP`;
+const connectionString = `mongodb+srv://${username}:${password}@ciandmp.duflp.mongodb.net/AppData?retryWrites=true&w=majority&appName=CIandMP`;
 
 mongoose.connect(connectionString)
     .then(() => { console.log('Connected to MongoDB'); })
     .catch((err) => { console.log(err); });
 
-const User = mongoose.models.User || mongoose.model('AppData', userSchema); 
+const User = mongoose.models.User || mongoose.model('Users', userSchema); 
 
 export const addUser = async (userData) => {
     try {
@@ -48,3 +48,60 @@ export const getUserById = async (userId) => {
         throw new Error('Failed to fetch user');
     }
 };
+
+export const addDepartments = async (userId, departments) => {
+    try {
+        const user = await User.findOne({ user_id: userId });
+        if (!user) {
+            throw new Error('User not found');
+        }
+        user.deps = [...new Set([...user.deps, ...departments])]; // Avoid duplicate entries
+        await user.save();
+        return user;
+    } catch (error) {
+        throw new Error('Failed to add departments');
+    }
+};
+
+export const addClubs = async (userId, clubs) => {
+    try {
+        const user = await User.findOne({ user_id: userId });
+        if (!user) {
+            throw new Error('User not found');
+        }
+        user.clubs = [...new Set([...user.clubs, ...clubs])]; // Avoid duplicate entries
+        await user.save();
+        return user;
+    } catch (error) {
+        throw new Error('Failed to add clubs');
+    }
+};
+
+export const removeDepartments = async (userId, departmentsToRemove) => {
+    try {
+        const user = await User.findOne({ user_id: userId });
+        if (!user) {
+            throw new Error('User not found');
+        }
+        user.deps = user.deps.filter(department => !departmentsToRemove.includes(department));
+        await user.save();
+        return user;
+    } catch (error) {
+        throw new Error('Failed to remove departments');
+    }
+};
+
+export const removeClubs = async (userId, clubsToRemove) => {
+    try {
+        const user = await User.findOne({ user_id: userId });
+        if (!user) {
+            throw new Error('User not found');
+        }
+        user.clubs = user.clubs.filter(club => !clubsToRemove.includes(club));
+        await user.save();
+        return user;
+    } catch (error) {
+        throw new Error('Failed to remove clubs');
+    }
+};
+
