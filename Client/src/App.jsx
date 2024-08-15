8a6c1d619b1d01efc7b2d5f3c4b0a96c9905d978
poseidon0z <adityaprabhu1290@gmail.com>
2024-08-15 23:37:08 +0530

    moved screens to right folder

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginForm from './components/LoginForm';
-import SignUpForm from './components/SignUpForm';
+import LoginForm from './screens/LoginForm';
+import SignUpForm from './screens/SignUpForm';
 
 function App() {
   return (
