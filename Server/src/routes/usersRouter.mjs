709f74a6fa876170b78cb2e3015d3b6659ed04cd
Azaken1248 <suvkrm@gmail.com>
2024-08-16 00:59:18 +0530

    Added Functionality To The User Routes

--- Server/src/routes/usersRouter.mjs
+++ Server/src/routes/usersRouter.mjs
@@ -1,18 +1,37 @@
 import express from 'express';
 
+import {addUser, removeUser, getUserById} from '../utils/userUtils.mjs'
+
 const userRouter = express.Router();
 
 
-userRouter.post('/add', (req, res) => {
-    res.send('Add user data');
+userRouter.post('/add', async (req, res) => {
+    try{
+        let userAdded = await addUser(req.body);
+        res.status(201).json(userAdded);
+        
+    }catch(error){
+        res.status(500).json({"error": error.message});
+    }
+    
 });
 
-userRouter.delete('/delete/:userId', (req, res) => {
-    res.send('Remove user data');
+userRouter.delete('/delete/:userId', async (req, res) => {
+    try{
+        let userDeleted = await removeUser(req.params.userId);
+        res.status(200).json(userDeleted);
+    }catch(error){
+        res.status(500).json({"error": error.message});
+    }
 });
 
-userRouter.get('/get/:userId', (req, res) => {
-    res.send('Get user info by ID');
+userRouter.get('/get/:userId', async (req, res) => {
+    try{
+        let user = await getUserById(req.params.userId);
+        res.status(200).json(user);
+    }catch(error){
+        res.status(500).json({"error": error.message});
+    }
 });
 
 export default userRouter;
