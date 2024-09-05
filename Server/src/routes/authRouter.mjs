cd83b64105dc3a71fcbbe5c1dc5c52d3c685c608
Azaken1248 <suvkrm@gmail.com>
2024-09-06 02:38:29 +0530

    Added Token Generation On SignUp And Login

--- Server/src/routes/authRouter.mjs
+++ Server/src/routes/authRouter.mjs
@@ -1,8 +1,36 @@
 import express from 'express';
-import { generateToken } from '../utils/jwtUtils.mjs';
-import { getUserByReg } from '../utils/userUtils.mjs';
+import crypto from 'crypto';
 
+import { generateToken } from '../utils/jwtUtils.mjs';
+import { addUser, getUserByReg } from '../utils/userUtils.mjs'; 
 const authRouter = express.Router();
+
+function hashPassword(password) {
+    return crypto.createHash('sha256').update(password).digest('hex');
+}
+
+authRouter.post('/signup', async (req, res) => {
+    const { regno, firstname, lastname, password } = req.body;
+    const user_id = "UID" + regno;
+
+    try {
+        const newUser = {
+            user_id,
+            reg_no: regno,
+            first_name: firstname,
+            last_name: lastname,
+            password_hash: hashPassword(password), 
+        };
+
+        await addUser(newUser);
+
+        const token = await generateToken({ user_id: newUser.user_id });
+        res.json({ token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 authRouter.post('/login', async (req, res) => {
     const { regno, password } = req.body;
@@ -14,14 +42,13 @@ authRouter.post('/login', async (req, res) => {
             return res.status(404).send('User not found');
         }
 
-        const isMatch = password === user.password_hash;
+        const isMatch = hashPassword(password) === user.password_hash;
 
         if (!isMatch) {
             return res.status(401).send('Invalid credentials');
         }
 
         const token = await generateToken({ user_id: user.user_id });
-
         res.json({ token });
     } catch (error) {
         console.log(error);
