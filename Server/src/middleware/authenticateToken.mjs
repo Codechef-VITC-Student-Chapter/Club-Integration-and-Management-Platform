a997e23562e2382912ff5e58e6a7a445f26c797f
poseidon0z <adityaprabhu1290@gmail.com>
2024-09-22 03:29:21 +0530

    Removed unnecessary logging in server

--- Server/src/middleware/authenticateToken.mjs
+++ Server/src/middleware/authenticateToken.mjs
@@ -1,16 +1,15 @@
 import { verifyToken } from '../utils/jwtUtils.mjs';
 
 export const authenticateToken = async (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
 
-    if (!token) return res.status(401).send('Access Denied');
+  if (!token) return res.status(401).send('Access Denied');
 
-    try {
-        const decoded = await verifyToken(token);
-        console.log(decoded);
-        next();
-    } catch (err) {
-        res.status(403).json({'error':'Invalid Token'});
-    }
+  try {
+    const decoded = await verifyToken(token);
+    next();
+  } catch (err) {
+    res.status(403).json({ error: 'Invalid Token' });
+  }
 };
