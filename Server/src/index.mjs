7f0d721a7d5388fab1aa73ed81b1e93e4f9c2dda
poseidon0z <adityaprabhu1290@gmail.com>
2024-09-22 07:41:12 +0530

    Added hello world endpoint for easy testing

--- Server/src/index.mjs
+++ Server/src/index.mjs
@@ -13,17 +13,17 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-
 app.use('/userApi', userRouter);
 app.use('/clubApi', clubRouter);
 app.use('/authApi', authRouter);
 app.use('/depsApi', depsRouter);
 app.use('/contApi', contRouter);
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 app.listen(3000, () => {
   console.log(`Server is running on port ${3000}`);
 });
-
-  
 
 export default app;
