e4f238c3d6f12255af80e10a6c8e0eca5169872e
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-10-23 17:18:43 +0530

    ui: login page on md updated

--- Client/src/screens/LoginForm.jsx
+++ Client/src/screens/LoginForm.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import Logo from '../assets/logo.png';
-import { PiUserCircle, PiLockLight } from 'react-icons/pi';
-import { useRunningContext } from '../contexts/RunningContext';
+import React, { useState } from "react";
+import Logo from "../assets/logo.png";
+import { PiUserCircle, PiLockLight } from "react-icons/pi";
+import { useRunningContext } from "../contexts/RunningContext";
+import loginImage from "../assets/loginScreen_Background.png";
 
-import SHA256 from 'crypto-js/sha256';
+import SHA256 from "crypto-js/sha256";
 function hashPassword(password) {
   return SHA256(password).toString();
 }
@@ -12,15 +13,15 @@ function LoginForm() {
   const { baseURL, setCurrentUser, currentUser, setToken } =
     useRunningContext();
 
-  const [regno, setRegno] = useState('');
-  const [password, setPassword] = useState('');
+  const [regno, setRegno] = useState("");
+  const [password, setPassword] = useState("");
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await fetch(`${baseURL}/authApi/login`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           regno: regno,
@@ -30,20 +31,23 @@ function LoginForm() {
       const data = await response.json();
 
       if (data.token) {
-        localStorage.setItem('token', data.token);
-        var payload = JSON.parse(window.atob(data.token.split('.')[1]));
+        localStorage.setItem("token", data.token);
+        var payload = JSON.parse(window.atob(data.token.split(".")[1]));
         setCurrentUser(payload.user_id);
         setToken(data.token);
       }
     } catch (error) {
-      console.log('Error in logging in! ' + error);
+      console.log("Error in logging in! " + error);
     }
   };
 
   return (
-    <div className="bg-cover bg-center bg-mobile-login md:bg-desktop-login">
-      <div className="min-h-screen flex items-center justify-center backdrop-blur-sm">
-        <div className="bg-white rounded-tl-[180px] md:rounded-[67px] shadow-lg p-8 w-full md:max-w-md md:w-[561px] md:h-[555px] mx-0 md:mx-4 md:relative absolute bottom-0 left-0 right-0 md:bottom-auto flex flex-col justify-center h-[85vh]">
+    <div className="bg-cover bg-center bg-[#E9F1FE] relative">
+      <div className="w-full absolute h-screen flex justify-center">
+        <img src={loginImage} alt="LoginImage" className="p-24 h-screen" />
+      </div>
+      <div className="min-h-screen flex items-center md:justify-end">
+        <div className="bg-white rounded-tl-[180px] md:rounded-[67px] shadow-lg p-8 w-full md:max-w-md md:w-[561px] md:h-[555px] mx-0 md:mx-4 md:relative absolute bottom-0 left-0 right-0 md:bottom-auto flex flex-col justify-center h-[85vh] border md:mr-16 px-12">
           <div className="w-full max-w-96 m-auto">
             <div className="flex justify-center mb-2">
               <img
@@ -52,13 +56,13 @@ function LoginForm() {
                 className="h-32"
               />
             </div>
-            <h2 className="text-2xl text-custom-blue font-bold text-center mb-8">
+            <h2 className="text-2xl text-custom-blue text-center mb-8">
               LOGIN
             </h2>
             <form onSubmit={handleSubmit}>
               <div className="mb-4">
-                <label className="block text-sm font-bold text-black mb-1">
-                  Register Number
+                <label className="block text-sm text-black mb-1">
+                  REGISTRATION NUMBER
                 </label>
                 <div className="flex items-center border-2 border-black rounded-md shadow-sm">
                   <div className="p-2">
@@ -67,7 +71,7 @@ function LoginForm() {
                   <input
                     type="regno"
                     name="regno"
-                    className="appearance-none block w-full pl-2 pr-3 py-2 placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                    className="appearance-none block w-full pl-2 pr-3 py-2 placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
                     placeholder="23ABC1234"
                     value={regno}
                     onChange={(e) => setRegno(e.target.value)}
@@ -75,8 +79,8 @@ function LoginForm() {
                 </div>
               </div>
               <div className="mb-4">
-                <label className="block text-sm font-bold text-black mb-1">
-                  Password
+                <label className="block text-sm text-black mb-1">
+                  PASSWORD
                 </label>
                 <div className="flex items-center border-2 border-black rounded-md shadow-sm">
                   <div className="p-2">
@@ -85,14 +89,14 @@ function LoginForm() {
                   <input
                     type="password"
                     name="password"
-                    className="appearance-none block w-full pl-2 pr-3 py-2 placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                    className="appearance-none block w-full pl-2 pr-3 py-2 placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
                     placeholder="********"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
               </div>
-              <div className="flex items-center justify-between mb-4">
+              <div className="flex items-center font-semibold justify-between mb-4">
                 <div className="flex items-center">
                   <input
                     type="checkbox"
@@ -115,17 +119,14 @@ function LoginForm() {
               <div>
                 <button
                   type="submit"
-                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-[43px] shadow-sm text-3xl font-medium text-white hover:bg-[#74baec] bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                  className="w-full flex justify-center p-1 border border-transparent rounded-md shadow-sm  text-white hover:bg-[#74baec] bg-[#4079DA] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                 >
                   Log in
                 </button>
               </div>
-              <div className="flex justify-between items-center mt-3 ml-2 mr-2">
+              <div className="flex font-semibold justify-end items-center mt-3">
                 <p className="text-sm text-black">Don’t have an account?</p>
-                <a
-                  href="/signup"
-                  className="font-medium text-custom-blue-darker hover:text-blue-500"
-                >
+                <a href="/signup" className=" ml-1 text-sm text-[#4079DA]">
                   Sign Up
                 </a>
               </div>
