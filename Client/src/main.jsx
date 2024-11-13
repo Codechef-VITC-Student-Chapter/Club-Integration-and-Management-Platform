d208496e00caa3b2ea0a7979b7e4ec2c873037c1
Sooraj Sunil <sooraj.namboothiry@gmail.com>
2024-11-13 21:58:27 +0530

    Successfully merged the new AdminView.jsx page

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import AdminView from './screens/AdminView.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <AdminView />
+    <App />
   </StrictMode>,
 )
