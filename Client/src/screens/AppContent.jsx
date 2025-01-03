e3f90a08ee2c814940b69adbbc37fbb4a2b7b81e
poseidon0z <adityaprabhu1290@gmail.com>
2025-01-03 14:13:54 +0530

    Fixed login default when no token present

--- Client/src/screens/AppContent.jsx
+++ Client/src/screens/AppContent.jsx
@@ -1,31 +1,31 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense, useState } from 'react';
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import LoadingScreen from "./LoadingScreen";
-import Navbar from "../components/Navbar";
-import { useRunningContext } from "../contexts/RunningContext";
-import Admin_view from "./Admin_view";
+import LoadingScreen from './LoadingScreen';
+import Navbar from '../components/Navbar';
+import { useRunningContext } from '../contexts/RunningContext';
+import Admin_view from './Admin_view';
 
-const PageNotFound = React.lazy(() => import("./PageNotFound"));
-const LoginForm = React.lazy(() => import("./LoginForm"));
-const SignUpForm = React.lazy(() => import("./SignUpForm"));
-const UploadDetails = React.lazy(() => import("./UploadDetails"));
-const SeeRequests = React.lazy(() => import("./SeeRequests"));
-const MemberDashboard = React.lazy(() => import("./MemberDashboard"));
+const PageNotFound = React.lazy(() => import('./PageNotFound'));
+const LoginForm = React.lazy(() => import('./LoginForm'));
+const SignUpForm = React.lazy(() => import('./SignUpForm'));
+const UploadDetails = React.lazy(() => import('./UploadDetails'));
+const SeeRequests = React.lazy(() => import('./SeeRequests'));
+const MemberDashboard = React.lazy(() => import('./MemberDashboard'));
 
 function AppContent() {
   const { token, isAdmin } = useRunningContext();
 
-  // if (!token) {
-  //   return (
-  //     <Routes>
-  //       <Route path="/loginform" element={<LoginForm />} />
-  //       <Route path="/signup" element={<SignUpForm />} />
-  //       <Route path="/*" element={<MemberDashboard />} />
-  //     </Routes>
-  //   );
-  // }
+  if (!token) {
+    return (
+      <Routes>
+        <Route path="/loginform" element={<LoginForm />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/*" element={<LoginForm />} />
+      </Routes>
+    );
+  }
   return (
     <>
       <Navbar />
