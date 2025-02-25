4ce62130993811ca1523d06c8a3d0ccfffb8f130
DISHA DANIEL <142607124+dishadaniellol@users.noreply.github.com>
2025-02-26 00:04:40 +0530

    Update tailwind.config.js

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,27 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      xs: { max: '350px' }, // Custom breakpoint for screens smaller than 330px
+      sm: '358px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       backgroundImage: {
-        "desktop-login": "url('./assets/desktop_login.png')",
-        "mobile-login": "url('./assets/mobile_login.png')",
+        'desktop-login': "url('./assets/desktop_login.png')",
+        'mobile-login': "url('./assets/mobile_login.png')",
       },
       fontFamily: {
-        lato: ["Lato", "sans-serif"],
-        mooli: ["Mooli", "sans-serif"],
-      },
-      colors: {
-        primary: "#E9F1FE",
-        secondary: "#2E3446",
-        "acc-primary": "#4079DA",
-        "acc-secondary": "#FFAC33",
-        black: "#333333",
-        "pitch-black": "#191919",
-        "other-green": "#20614C",
-        negative: "#DC3545",
-        neutral: "#FFC107",
-        positive: "#198754",
+        lato: ['Lato', 'sans-serif'],
+        mooli: ['Mooli', 'sans-serif'],
       },
     },
   },
