7bd77f5cba50960a3366f61d3040e64cbaf2ce0f
Sasank-V <sasanklearner@gmail.com>
2025-06-27 20:43:02 +0530

    fix: vercel deploy

--- Client/src/screens/Points-Summary/components/Plus.jsx
+++ Client/src/screens/Points-Summary/components/Plus.jsx
@@ -1,10 +1,12 @@
-import React from "react"
-import add from "../assets/Plus2.jpg"
+import add from "../assets/Plus2.jpg";
 
 function Plus() {
-    return (
-        <img src={add} className=" h-[43px] w-[45px] max-md:h-[23px] max-md:w-[25px] rounded-tr-l max-md:rounded-tr-md " />
-    );
+  return (
+    <img
+      src={add}
+      className=" h-[43px] w-[45px] max-md:h-[23px] max-md:w-[25px] rounded-tr-l max-md:rounded-tr-md "
+    />
+  );
 }
 
 export default Plus;
