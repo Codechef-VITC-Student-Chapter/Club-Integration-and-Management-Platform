8cae85dec15d5ac3f98a718f998bfeb1b24badff
dishadaniellol <142607124+dishadaniellol@users.noreply.github.com>
2025-09-06 23:44:09 +0530

    feat: add members page

--- client-next/components/layouts/sidebar/sidebar-wrapper.tsx
+++ client-next/components/layouts/sidebar/sidebar-wrapper.tsx
@@ -1,3 +1,4 @@
+// components/layouts/sidebar/sidebar-wrapper.tsx
 import { AppSidebar } from "./sidebar";
 import { Separator } from "@/components/ui/separator";
 import {
@@ -11,18 +12,27 @@ interface SidebarWrapperProps {
   pageTitle: string;
 }
 
-export function SidebarWrapper({ children, pageTitle }: SidebarWrapperProps) {
+function SidebarWrapper({ children, pageTitle }: SidebarWrapperProps) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset>
+
+      {/* SidebarInset must be full-height / flex column so children can flex correctly */}
+      <SidebarInset className="flex-1 min-h-0 flex flex-col m-0 p-0">
+        {/* top appbar */}
         <div className="flex h-16 items-center gap-2 px-4 border-b">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
           <h1 className="text-xl font-semibold text-foreground">{pageTitle}</h1>
         </div>
-        <div className="flex flex-1 flex-col gap-4 w-full h-full">{children}</div>
+
+        <div className="flex-1 min-h-0 flex flex-col m-0 p-0">
+          {children}
+        </div>
       </SidebarInset>
     </SidebarProvider>
   );
 }
+
+export { SidebarWrapper };
+export default SidebarWrapper;
